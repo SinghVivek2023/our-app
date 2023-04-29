2fc79c91fa6dbf3c9ac51d6f2dbb90f61e79271e
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-04-29 10:56:37 +0530

    added data for chapters.js and made selecting chapter  after selecting standard and subjects

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, { useState } from "react";
 import Select from "react-select";
 import standard from "./data/standard";
 import subjects from "./data/subjects";
+import chapters from "./data/chapters";
 
 export default function App() {
   const [selectedStandard, setSelectedStandard] = useState(null);
   const [selectedSubject, setSelectedSubject] = useState(null);
-
   const standardOptions = standard.map((std) => {
     return {
       value: std.value,
       label: std.label,
     };
   });
-
   const subjectOptions = subjects
-    .filter((subject) => subject.standardId === selectedStandard?.value)
+    .filter((subject) => {
+      return subject.standardId === selectedStandard?.value;
+    })
     .map((subject) => {
       return {
         value: subject.id,
         label: subject.name,
+        isDisabled: !selectedStandard,
+      };
+    });
+  const chapterOptions = chapters
+    .filter((chapter) => {
+      return chapter.subjectId === selectedSubject?.value;
+    })
+    .map((chapter) => {
+      return {
+        value: chapter.id,
+        label: chapter.name,
+        isDisabled: !selectedSubject,
       };
     });
 
@@ -28,19 +41,21 @@ export default function App() {
       <div style={{ margin: 20, width: 500 }}>
         <Select
           options={standardOptions}
-          value={selectedStandard}
           placeholder="Select Standard"
+          value={selectedStandard}
           onChange={setSelectedStandard}
         />
       </div>
       <div style={{ margin: 20, width: 500 }}>
         <Select
           options={subjectOptions}
-          value={selectedSubject}
           placeholder="Select Subject"
+          value={selectedSubject}
           onChange={setSelectedSubject}
-          isDisabled={!selectedStandard}
         />
+      </div>
+      <div style={{ margin: 20, width: 500 }}>
+        <Select options={chapterOptions} placeholder="Select Chapter" />
       </div>
     </>
   );
