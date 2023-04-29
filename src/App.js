b6ca6f3d2e7573a91d3d8dbd2622d0804976baea
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-04-29 10:17:33 +0530

    added dummy data for subjects in data folder and imported in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 import standard from "./data/standard";
+import subjects from "./data/subjects";
 
 export default function App() {
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const subjectOptions = subjects.map((subject) => {
+    return {
+      value: subject.id,
+      label: subject.name,
+    };
+  });
   return (
-    <div style={{ margin: 20, width: 500 }}>
-      <Select options={standard} placeholder="select standard" />
-    </div>
+    <>
+      <div style={{ margin: 20, width: 500 }}>
+        <Select options={standard} placeholder="select standard" />
+      </div>
+      <div style={{ margin: 20, width: 500 }}>
+        <Select
+          options={subjectOptions}
+          value={selectedSubject}
+          placeholder="Select Subject"
+          onChange={setSelectedSubject}
+        />
+      </div>
+    </>
   );
 }
