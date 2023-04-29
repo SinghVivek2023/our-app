242068582f44f850ea595dcbbb3bf6314ef7b08e
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-04-29 10:19:10 +0530

    updated standard data

--- src/data/standard.js
+++ src/data/standard.js
@@ -1,13 +1,13 @@
 const standard = [
-  { standardId: "1", label: "Class 1" },
-  { standardId: "2", label: "Class 2" },
-  { standardId: "3", label: "Class 3" },
-  { standardId: "4", label: "Class 4" },
-  { standardId: "5", label: "Class 5" },
-  { standardId: "6", label: "Class 6" },
-  { standardId: "7", label: "Class 7" },
-  { standardId: "8", label: "Class 8" },
-  { standardId: "9", label: "Class 9" },
-  { standardId: "10", label: "Class 10" },
+  { value: "1", label: "Class 1" },
+  { value: "2", label: "Class 2" },
+  { value: "3", label: "Class 3" },
+  { value: "4", label: "Class 4" },
+  { value: "5", label: "Class 5" },
+  { value: "6", label: "Class 6" },
+  { value: "7", label: "Class 7" },
+  { value: "8", label: "Class 8" },
+  { value: "9", label: "Class 9" },
+  { value: "10", label: "Class 10" },
 ];
 export default standard;
