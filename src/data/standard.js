376805a4e6d10be40c2350beaadaf3eac7a8f488
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-04-29 10:02:06 +0530

    added dummy data for standard in data  folder contains standard.js  and install neccessary npm

---
+++ src/data/standard.js
@@ -0,0 +1,13 @@
+const standard = [
+  { value: "1", label: "Class 1" },
+  { value: "2", label: "Class 2" },
+  { value: "3", label: "Class 3" },
+  { value: "4", label: "Class 4" },
+  { value: "5", label: "Class 5" },
+  { value: "6", label: "Class 6" },
+  { value: "7", label: "Class 7" },
+  { value: "8", label: "Class 8" },
+  { value: "9", label: "Class 9" },
+  { value: "10", label: "Class 10" },
+];
+export default standard;
