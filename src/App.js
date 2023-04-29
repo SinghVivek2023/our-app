a600dc022a0549fa862d241c89c131461ab102aa
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-04-29 11:12:02 +0530

    added data for topics.js and update code in app.js  for selecting topics  and downgraded the react-router-dom for convience

--- src/App.js
+++ src/App.js
@@ -3,44 +3,45 @@ import Select from "react-select";
 import standard from "./data/standard";
 import subjects from "./data/subjects";
 import chapters from "./data/chapters";
+import topics from "./data/topics";
 
 export default function App() {
   const [selectedStandard, setSelectedStandard] = useState(null);
   const [selectedSubject, setSelectedSubject] = useState(null);
-  const standardOptions = standard.map((std) => {
-    return {
-      value: std.value,
-      label: std.label,
-    };
-  });
+  const [selectedChapter, setSelectedChapter] = useState(null);
+
+  // Filter subjects based on selected standard
   const subjectOptions = subjects
-    .filter((subject) => {
-      return subject.standardId === selectedStandard?.value;
-    })
-    .map((subject) => {
-      return {
-        value: subject.id,
-        label: subject.name,
-        isDisabled: !selectedStandard,
-      };
-    });
+    .filter((subject) => subject.standardId === selectedStandard?.value)
+    .map((subject) => ({
+      value: subject.id,
+      label: subject.name,
+      isDisabled: !selectedStandard,
+    }));
+
+  // Filter chapters based on selected subject
   const chapterOptions = chapters
-    .filter((chapter) => {
-      return chapter.subjectId === selectedSubject?.value;
-    })
-    .map((chapter) => {
-      return {
-        value: chapter.id,
-        label: chapter.name,
-        isDisabled: !selectedSubject,
-      };
-    });
+    .filter((chapter) => chapter.subjectId === selectedSubject?.value)
+    .map((chapter) => ({
+      value: chapter.id,
+      label: chapter.name,
+      isDisabled: !selectedSubject,
+    }));
+
+  // Filter topics based on selected chapter
+  const topicOptions = topics
+    .filter((topic) => topic.chapterId === selectedChapter?.value)
+    .map((topic) => ({
+      value: topic.id,
+      label: topic.name,
+      isDisabled: !selectedChapter,
+    }));
 
   return (
     <>
       <div style={{ margin: 20, width: 500 }}>
         <Select
-          options={standardOptions}
+          options={standard}
           placeholder="Select Standard"
           value={selectedStandard}
           onChange={setSelectedStandard}
@@ -52,10 +53,24 @@ export default function App() {
           placeholder="Select Subject"
           value={selectedSubject}
           onChange={setSelectedSubject}
+          isDisabled={!selectedStandard}
         />
       </div>
       <div style={{ margin: 20, width: 500 }}>
-        <Select options={chapterOptions} placeholder="Select Chapter" />
+        <Select
+          options={chapterOptions}
+          placeholder="Select Chapter"
+          value={selectedChapter}
+          onChange={setSelectedChapter}
+          isDisabled={!selectedSubject}
+        />
+      </div>
+      <div style={{ margin: 20, width: 500 }}>
+        <Select
+          options={topicOptions}
+          placeholder="Select Topic"
+          isDisabled={!selectedChapter}
+        />
       </div>
     </>
   );
