cd9865cb5c8b56b189b80d859e7f2598bc77a5a4
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-04-29 10:35:02 +0530

    updated data of subject.js and made code select subject after selecting standard on the basic of value of standard

--- src/App.js
+++ src/App.js
@@ -4,17 +4,34 @@ import standard from "./data/standard";
 import subjects from "./data/subjects";
 
 export default function App() {
+  const [selectedStandard, setSelectedStandard] = useState(null);
   const [selectedSubject, setSelectedSubject] = useState(null);
-  const subjectOptions = subjects.map((subject) => {
+
+  const standardOptions = standard.map((std) => {
     return {
-      value: subject.id,
-      label: subject.name,
+      value: std.value,
+      label: std.label,
     };
   });
+
+  const subjectOptions = subjects
+    .filter((subject) => subject.standardId === selectedStandard?.value)
+    .map((subject) => {
+      return {
+        value: subject.id,
+        label: subject.name,
+      };
+    });
+
   return (
     <>
       <div style={{ margin: 20, width: 500 }}>
-        <Select options={standard} placeholder="select standard" />
+        <Select
+          options={standardOptions}
+          value={selectedStandard}
+          placeholder="Select Standard"
+          onChange={setSelectedStandard}
+        />
       </div>
       <div style={{ margin: 20, width: 500 }}>
         <Select
@@ -22,6 +39,7 @@ export default function App() {
           value={selectedSubject}
           placeholder="Select Subject"
           onChange={setSelectedSubject}
+          isDisabled={!selectedStandard}
         />
       </div>
     </>
