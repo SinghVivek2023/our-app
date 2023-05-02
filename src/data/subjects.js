5ec54ca10d4803fb7c5c19046a209ab7e6932947
SinghVivek2023 <113018935+SinghVivek2023@users.noreply.github.com>
2023-05-02 12:52:49 +0530

    updated the data of subject and chapter  like Enviromental Studies was attached together. seperated by " "

--- src/data/subjects.js
+++ src/data/subjects.js
@@ -1,13 +1,13 @@
 const subjects = {
   "Class 1st": ["Maths", "Science", "English"],
-  "Class 2nd": ["EnviromentalStudies", "Science", "English"],
+  "Class 2nd": ["Enviromental Studies", "Science", "English"],
   "Class 3rd": ["Physcis", "Chemistry", "Biology"],
   "Class 4th": ["History", "Geography"],
   "Class 5th": ["Algebra", "Geometry"],
-  "Class 6th": ["ComputerScience", "InformationTechnology"],
-  "Class 7th": ["Economics", "PoliticalScience", "English"],
+  "Class 6th": ["Computer Science", "Information Technology"],
+  "Class 7th": ["Economics", "Political Science", "English"],
   "Class 8th": ["Accountancy", "Business Study", "Sanskrit"],
-  "Class 9th": ["Statistics", "AppliedMathematics", "Biology"],
-  "Class 10th": ["Webdevelopment", "ComputerScience", "Hindi"],
+  "Class 9th": ["Statistics", "Applied Mathematics", "Biology"],
+  "Class 10th": ["Web development", "Computer Science", "Hindi"],
 };
 export default subjects;
